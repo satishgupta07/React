b5a44440e4744a853178edba4e4e18cba8753dbe
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-09-28 11:18:57 +0530

    Create React Projects | Understanding the react flow and structure
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ 01_basic_react/src/App.js
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Chai />
+    </>
+  );
+}
+
+export default App;
---
+++ 01_vite_react/src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from "./Chai"
+
+function App() {
+  return (
+    <>
+      <h1>Chai aur React with Vite</h1>
+      <Chai />
+    </>
+  )
+}
+
+export default App
