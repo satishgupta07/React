1f92ed37e6c12e6361f5feb0ca23225ab99ecbf3
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-10-10 12:52:51 +0530

    Interview Question on Counter
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -10,8 +10,17 @@ function App() {
   // let counter = 15
 
   const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter)
+    // counter = counter + 1;
+
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
 
   const removeValue = () => {
