45d94146683c08154710df455fffa42fc736c5af
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2023-10-10 12:39:58 +0530

    Tailwind CSS | Props
    Signed-off-by: Satish Kumar Gupta <satishgupta.cse01@gmail.com>

---
+++ 03_tailwind_props/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />
+    </>
+  )
+}
+
+export default App
